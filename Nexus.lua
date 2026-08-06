41ef52025a2ac34039c0a57c037a05673a5d38b4
Mudds18 <roplayzgamer@gmail.com>
2026-08-07 01:30:19 +0200

    Update Nexus.lua

--- Nexus.lua
+++ Nexus.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Mudds18/test/refs/heads/main/Nexus-terminal.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Mudds18/test/refs/heads/main/NexusUltimate.lua"))()
